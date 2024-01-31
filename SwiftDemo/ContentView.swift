1e33890467043a3a6b0e0e352442eca7c66dca31
Atlakyn <52728316+Atlakyn@users.noreply.github.com>
2024-01-31 17:25:55 +0800

    update: 提交更新

--- SwiftDemo/ContentView.swift
+++ SwiftDemo/ContentView.swift
@@ -9,16 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("今天吃什么?")
+                .font(.headline)
+            Text("What to eat today?")
+                .font(.system(.headline, design: .rounded))
         }
+        .foregroundColor(Color.orange)
         .padding()
+        .contextMenu(){
+            HStack{
+                Button(action: {
+                    let pasteboard = UIPasteboard.general
+                    pasteboard.string = "今天吃什么?"
+                }){
+                    HStack {
+                        List {
+                            Text("chicken")
+                            Text("fucking cazzy Thursday")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView2: View {
+    var body: some View {
+        VStack{
+            Text("hello world")
+                .padding()
+            Circle()
+                .padding()
+        }
+        
+    }
 }
+
+struct DemoView_Previews: PreviewProvider{
+    static var previews: some View{
+        Group{
+            ContentView()
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.light)
+            ContentView()
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
--- SwiftDemo/SwiftDemoApp.swift
+++ SwiftDemo/SwiftDemoApp.swift
@@ -12,6 +12,7 @@ struct SwiftDemoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+            ContentView2()
         }
     }
 }
